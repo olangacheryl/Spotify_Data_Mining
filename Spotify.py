dce6c8765901099ee54aecf61aa6070b38e0d0fd
olangacheryl <91678812+olangacheryl@users.noreply.github.com>
2024-04-22 19:20:13 -0400

    Add files via upload

--- Spotify.py
+++ Spotify.py
@@ -12,14 +12,15 @@ import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy import oauth2
+import csv 
 
 # Insert your Spotify username and the credentials that you obtained from spotify developer
 cid = '5370c0ca87054f429022354f75b83c9f'
 secret = '8931c1ca07414c1c961cb8660939ac92'
-redirect_uri='http://localhost:3000/analysis'
+redirect_uri="http://localhost:3000/analysis"
 username = 'hadasa'
 
-# Once the Authorisation is complete, we just need to `sp` to call the APIs
+#Authorization and sp to call the APIs
 scope = 'user-top-read playlist-modify-private playlist-modify-public'
 token = util.prompt_for_user_token(username, scope, client_id=cid, client_secret=secret, redirect_uri=redirect_uri)
 
@@ -29,125 +30,47 @@ else:
     print("Can't get token for", username)
 
 
-# Getting features for each song
-def fetch_audio_features(sp, df):
-    playlist = df[['track_id','track_name']] 
-    index = 0
-    audio_features = []
+
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f&nd=1&dlsi=6ce05bd2564c4195"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+    #URI
+    track_uri = track["track"]["uri"]
     
-    # Make the API request
-    while index < playlist.shape[0]:
-        audio_features += sp.audio_features(playlist.iloc[index:index + 50, 0])
-        index += 50
+    #Track name
+    track_name = track["track"]["name"]
     
-    # Create an empty list to feed in different charactieritcs of the tracks
-    features_list = []
-    #Create keys-values of empty lists inside nested dictionary for album
-    for features in audio_features:
-        features_list.append([features['danceability'],
-                              features['acousticness'],
-                              features['energy'], 
-                              features['tempo'],
-                              features['instrumentalness'], 
-                              features['loudness'],
-                              features['liveness'],
-                              features['duration_ms'],
-                              features['key'],
-                              features['valence'],
-                              features['speechiness'],
-                              features['mode']
-                             ])
+    #Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = sp.artist(artist_uri)
     
-    df_audio_features = pd.DataFrame(features_list, columns=['danceability', 'acousticness', 'energy','tempo', 
-                                                             'instrumentalness', 'loudness', 'liveness','duration_ms', 'key',
-                                                             'valence', 'speechiness', 'mode'])
+    #Name, popularity, genre
+    artist_name = track["track"]["artists"][0]["name"]
+    artist_pop = artist_info["popularity"]
+    artist_genres = artist_info["genres"]
+    # print(artist_genres)
+    #Album
+    album = track["track"]["album"]["name"]
     
-    # Create the final df, using the 'track_id' as index for future reference
-    df_playlist_audio_features = pd.concat([playlist, df_audio_features], axis=1)
-    df_playlist_audio_features.set_index('track_name', inplace=True, drop=True)
-    return df_playlist_audio_features
+    #Popularity of the track
+    track_pop = track["track"]["popularity"]
+
+    df= pd.DataFrame(columns= [track_name,  artist_name, artist_pop,  artist_genres])
+    data = [track_name, artist_name, artist_pop, artist_genres]
+    print(data)
+    
+# with open("playlist_song0.csv", mode="w", encoding="utf-8") as file:
+#     reader = csv.reader(file)
+#     data = [track_name, artist_name, artist_pop, artist_genres]
+
+#     writer = csv.writer(file)
+#     writer.writerow(["track", "artist", "pop", "genre"])
+#     writer.writerow(df)
+
+  
+    # writer.writerow([track_name, artist_name, artist_pop ,artist_genres])
+   
 
 
-
-# Getting playlist IDs from each of Spotify's playlists
-playlists = sp.user_playlists('spotify')
-spotify_playlist_ids = []
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        spotify_playlist_ids.append(playlist['uri'][-22:])
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-(spotify_playlist_ids[:20])
-
-
-len(spotify_playlist_ids)
-
-# Creating a function to get the first 50 tracks IDs from a playlist
-def getTrackIDs(playlist_id):
-    playlist = sp.user_playlist('spotify', playlist_id)
-    for item in playlist['tracks']['items'][:50]:
-        track = item['track']
-        ids.append(m1[i]) #changed from "id" to "track_id"
-    return
-
-# Creating a function get features of each track from track id
-def getTrackFeatures(track_id):
-  meta = sp.track(track_id)
-  features = sp.audio_features(track_id)
-
-  # meta
-  track_id = track_id
-  name = meta['name']
-  album = meta['album']['name']
-  artist = meta['album']['artists'][0]['name']
-  release_date = meta['album']['release_date']
-  length = meta['duration_ms']
-  popularity = meta['popularity']
-
-  # features
-  acousticness = features[0]['acousticness']
-  danceability = features[0]['danceability']
-  energy = features[0]['energy']
-  instrumentalness = features[0]['instrumentalness']
-  liveness = features[0]['liveness']
-  loudness = features[0]['loudness']
-  speechiness = features[0]['speechiness']
-  tempo = features[0]['tempo']
-  time_signature = features[0]['time_signature']
-
-  #changes made by cheryl
-  track = []
-  m1= [track_id, name, album, artist, release_date, length, popularity, danceability, acousticness, energy, instrumentalness, liveness, loudness, speechiness, tempo, time_signature]
-  for i in m1:
-      track.append(i)
-  return track
-
-# # # %%time
-# Gathering track ids
-ids = []
-for x in spotify_playlist_ids[:200]:
-    getTrackIDs(x)
-ids[:5]
-
-
-
-# # # %%time
-# loop over track ids to get audio features for each track
-tracks = []
-for i in range(len(ids)):
-    try:  
-        track = getTrackFeatures(ids[i])
-        tracks.append(track)
-    except:
-        pass
-
-# create dataset
-df = pd.DataFrame(tracks, columns = ['track_id', 'name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability', 'acousticness', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'time_signature'])
-df.head()
-
-df.to_csv('playlist_songs.csv',index=False)
-
-df = pd.read_csv('data/playlist_songs.csv')
-df.head()
